f37294d6fd5698c9426953f637e36d16bbddffaa
junjuny0227 <junjuny0227@gmail.com>
2026-03-12 12:04:37 +0900

    feat: Dockerfile 및 next.config.mjs 수정으로 환경 변수 추가 및 빌드 프로세스 개선

--- apps/client/next.config.mjs
+++ apps/client/next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const nextConfig = {
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../../'),
-  },
   images: {
     domains: ['github.com'],
     remotePatterns: [
