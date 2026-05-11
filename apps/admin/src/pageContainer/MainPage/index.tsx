7b540b73c9529e8810c8313a247bdd8afb181c3e
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2026-05-11 17:02:27 +0900

    feat: 파라미터 조건 추가

--- apps/admin/src/pageContainer/MainPage/index.tsx
+++ apps/admin/src/pageContainer/MainPage/index.tsx
@@ -66,7 +66,7 @@ const MainPage = ({
       size: PER_PAGE,
       testResultTag: testResultTag === 'REQUEST' ? 'ALL' : testResultTag,
       screeningTag: screeningTag,
-      isSubmitted: isSubmitted,
+      isSubmitted: testResultTag === 'REQUEST' ? 'ALL' : isSubmitted,
       keyword: debouncedKeyword,
       status: testResultTag === 'REQUEST' ? requested : undefined,
     },
